b7f6cc739d023fb729e5fd28f322c38f4cd2998c
Frank Albanese <albanesefc9@gmail.com>
2022-04-26 22:05:22 -0400

    Added banner - improved staking layout

--- src/app/stake/stake.component.ts
+++ src/app/stake/stake.component.ts
@@ -1,27 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import * as solanaWeb3 from '@solana/web3.js';
+import { EventEmitter } from 'stream';
 
 declare global {
   interface Window {
       solana: any;
   }
 }
-
 const isPhantomInstalled: boolean = window.solana && window.solana.isPhantom;
-
-const connect = async (): Promise<String> => {
-  {
-  try {
-    const resp = await window.solana.connect();
-    const result = resp.publicKey.toString()
-    window.alert(`${result} is connected and blockcahin ready`);
-    return result;
-} catch (err) {
-    return "${err}";
-}
-  }
-}
-
 
 const getProvider = () => {
   if ("solana" in window) {
@@ -43,8 +29,19 @@ export class StakeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    connect();
+  }
 
+  async connect(): Promise<String> {
+    {
+    try {
+      const resp = await window.solana.connect();
+      const result = resp.publicKey.toString()
+      window.alert(`${result} is connected and blockcahin ready`);
+      return result;
+  } catch (err) {
+      return "${err}";
+  }
+    }
   }
 
 }
